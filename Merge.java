8409d6b37a55df82b18e55938da4eb16c96fba1c
Ilya Sashcheka <sas4eka@gmail.com>
2018-03-17 22:58:12 +0100

    improve split and merge

--- Merge.java
+++ Merge.java
@@ -2,13 +2,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Merge {
+	static final String inputFormat = ".in";
+	static final String outputFormat = ".out";
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		String task = "example";
 		for (int k = 1; k <= 10; k++) {
-			String testfile = String.format("%s%02d.in", task, k);
+			String testfile = String.format("%s%02d" + inputFormat, task, k);
 			Reader reader = new Reader(testfile);
-			PrintWriter writer = new PrintWriter(String.format("%s%02d.ans", task, k));
+			PrintWriter writer = new PrintWriter(String.format("%s%02d" + outputFormat, task, k));
 			int tests = reader.nextInt();
 			for (int t = 0; t < tests; t++) {
 
@@ -18,16 +20,16 @@ public class Merge {
 				// reader.nextInt();
 				// }
 
-				String partfile = String.format("%s%02d_%04d.ans", task, k, t);
-				Reader partreader = new Reader(partfile);
+				String partFile = String.format("%s%02d_%04d" + outputFormat, task, k, t);
+				Reader partReader = new Reader(partFile);
 
 				// TODO: read answers from parts and merge them together
 				// for (int i = 0; i < n; i++) {
-				// int x = partreader.nextInt();
+				// int x = partReader.nextInt();
 				// writer.println(x);
 				// }
 
-				partreader.close();
+				partReader.close();
 			}
 			writer.close();
 			reader.close();
--- Split.java
+++ Split.java
@@ -2,24 +2,25 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Split {
+	static final String inputFormat = ".in";
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		String task = "example";
 		for (int k = 0; k <= 10; k++) {
-			String testfile = String.format("%s%02d.in", task, k);
+			String testfile = String.format("%s%02d" + inputFormat, task, k);
 			Reader reader = new Reader(testfile);
 			int tests = reader.nextInt();
 			for (int t = 0; t < tests; t++) {
-				String partfile = String.format("%s%02d_%04d.in", task, k, t);
+				String partFile = String.format("%s%02d_%04d" + inputFormat, task, k, t);
 				System.out.print(String.format("\"%s%02d_%04d\"", task, k, t) + ", ");
-				PrintWriter writer = new PrintWriter(partfile);
+				PrintWriter partWriter = new PrintWriter(partFile);
 
 				// TODO: read from reader and write to separate writers
 				// int m = reader.nextInt();
 				// int n = reader.nextInt();
-				// writer.println(m + " " + n);
+				// partWriter.println(m + " " + n);
 
-				writer.close();
+				partWriter.close();
 			}
 			reader.close();
 		}
